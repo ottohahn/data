294136ce7bb1188b84e834b535be01f6a3553872
vabraham <vabraham24@gmail.com>
2016-08-17 16:14:05 -0700

    added chained function

--- edu_exp/experience.py
+++ edu_exp/experience.py
@@ -10,7 +10,7 @@ from collections import OrderedDict
 
 dates_zero=re.compile(ur'[0-9]{4}\s?-\s?[0-9]{4}|[0-9]{2}/[0-9]{2,4}|Jan|Feb|Mar|March|Apr|May|June|July|Aug|Sept|Oct|Nov|Dec [0-9]{4} -|[0-9]{4}')
 
-dates_one = re.compile(ur'[A-Z]+\s[0-9]{2,4}(\t|\s)*(-|~|to|through)(\t|\s)*(Present|Current|[A-Z]+\s[0-9]{2,4})|[0-9]{4}(\t|\s)*(-|~|to|through)(\t|\s)*(Present|Current|[0-9]{4})|[0-9]{,2}(\.|/)[0-9]{2,4}(\t|\s)*(-|~|to|through)(\t|\s)*(Present|Current|[0-9]{,2}(\.|/)[0-9]{2,4})|[A-Z]+(\t|\s)*[0-9]{4}(\t|\s)*[A-Z]+(\t|\s)*[0-9]{4}', re.IGNORECASE)
+dates_one = re.compile(ur'[A-Z]+\s?[0-9]{2,4}(\t|\s)*(-|~|to|through)(\t|\s)*(Present|Current|[A-Z]+\s?[0-9]{2,4})|[0-9]{4}(\t|\s)*(-|~|to|through)(\t|\s)*(Present|Current|[0-9]{4})|[0-9]{,2}(\.|/)[0-9]{2,4}(\t|\s)*(-|~|to|through)(\t|\s)*(Present|Current|[0-9]{,2}(\.|/)[0-9]{2,4})|[A-Z]+(\t|\s)*[0-9]{4}(\t|\s)*[A-Z]+(\t|\s)*(Present|Current|[0-9]{4})', re.IGNORECASE)
 
 headerstrt = re.compile(ur'Experience\n|Experience\s\t\n|EXPERIENCE|WORK EXPERIENCE|EMPLOYMENT|PROFESSIONAL BACKGROUND|PROFESSIONAL EXPERIENCE|JOBS') #Need to deal with orthographic errors
 
@@ -126,6 +126,7 @@ def convert_dates(date_list, cur_date=None):
         for group in date_list:
             group = re.sub(r"(\t|\n|\.)", " ", group)
             lst = re.split(ur"(-|~|\s\s|\s(-|~|to|through)\s)", group)
+
             beg = parser.parse(lst[0])
             if lst[-1].lower() in ['present', 'current']:
                 end = cur_date
@@ -147,6 +148,7 @@ def remove_overlap(converted_dates):
                     line[0] = x[1]
                     converted_dates[i-1] = tuple(line)
                 i += 1
+    return converted_dates
 
 def get_yrs(no_ov_dates):
     if not no_ov_dates:
@@ -155,6 +157,17 @@ def get_yrs(no_ov_dates):
         new_list = []
         for group in no_ov_dates:
             new_list.append((group[1]-group[0]).days)
-        return float(np.sum(new_list))/365
+        yrs = float(np.sum(new_list))/365
+        if yrs < 0:
+            return 0
+        else:
+            return yrs
 
-# def chain_exp()
+def chain_exp(resume, cur_date=None):
+    lst = get_exp_zero(resume)
+    clean_lst = get_exp_one(lst)
+    converted_dts = convert_dates(clean_lst, cur_date)
+    rem_ov_lst = remove_overlap(converted_dts)
+    yrs = get_yrs(rem_ov_lst)
+    return yrs
+
